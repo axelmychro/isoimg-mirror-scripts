f855cd993726d8f6ec55e4e411b3a14ae5d247aa
Axel <axelmychro@gmail.com>
2026-06-15 11:44:28 +0700

    Append readonly to variables

--- ubuntu-0-latest-desktop.sh
+++ ubuntu-0-latest-desktop.sh
@@ -11,8 +11,14 @@ latest_version="$(list "$path" |
 readonly latest_version
 log "Latest $latest_version"
 
-iso_file="$(list "$path/$latest_version/" | awk '/\.iso$/ && /desktop/ {print $NF}')"
+iso_file="$(list "$path/$latest_version/" |
+	awk -- '/\.iso$/ && /desktop/ {print $NF}' |
+	sort -V -- |
+	tail -n1 --)"
+readonly iso_file
 log "Found $iso_file"
 
-src="$path/$latest_version/$iso_file"
+readonly src="$path/$latest_version/$iso_file"
+log "$src"
+
 download "$src"
