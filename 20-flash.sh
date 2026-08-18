8cb2bfd2c6af5bc46d14b81fc564e5f6cac69e57
Axel <axelmychro@gmail.com>
2026-08-18 20:57:08 +0700

    update flash script

--- 20-flash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-readonly src="${1:?no src}"
-readonly out="${2:?no out}"
-
-sudo dd bs=4M if="$src" of="$out" status=progress oflag=sync
--- flash.sh
+++ flash.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -eu
+set -eux
 
 readonly src="${1:?no src}"
 readonly out="${2:?no out}"
-
 sudo dd bs=4M if="$src" of="$out" status=progress oflag=sync
